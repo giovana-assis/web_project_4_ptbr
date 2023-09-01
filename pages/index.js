42056a7481e81c0e8a3e4909b7d1b868938b7660
giovana.assis <giovana.assis@live.com>
2023-09-01 17:55:49 -0300

    Fix: indentation

--- pages/index.js
+++ pages/index.js
@@ -1,32 +1,29 @@
+const openFormButton = document.querySelector(".edit-button");
+const popup = document.querySelector(".popup");
+const closeButton = popup.querySelector(".popup__close");
 
-const openFormButton = document.querySelector('.edit-button');
-const popup = document.querySelector('.popup');
-const closeButton = popup.querySelector('.popup__close');
-
-function addPopup(){
-    popup.classList.add('popup-visible');
+function addPopup() {
+  popup.classList.add("popup-visible");
 }
 
-function closePopup(){
-    popup.classList.remove('popup-visible');
+function closePopup() {
+  popup.classList.remove("popup-visible");
 }
 
-openFormButton.addEventListener('click', addPopup);
-closeButton.addEventListener('click', closePopup);  
+openFormButton.addEventListener("click", addPopup);
+closeButton.addEventListener("click", closePopup);
 
-const profileName = document.querySelector('.profile__name');
-const profileDetail = document.querySelector('.profile__detail');
+const profileName = document.querySelector(".profile__name");
+const profileDetail = document.querySelector(".profile__detail");
 
-const inputName = document.querySelector('.popup__name');
-const inputDetail = document.querySelector('.popup__detail');
-const saveButton = document.querySelector('.popup__form');
+const inputName = document.querySelector(".popup__name");
+const inputDetail = document.querySelector(".popup__detail");
+const saveButton = document.querySelector(".popup__form");
 
 function changeProfile(e) {
-    e.preventDefault();
-          profileName.textContent = inputName.value;
-        profileDetail.textContent = inputDetail.value;
-    closePopup();
+  e.preventDefault();
+  profileName.textContent = inputName.value;
+  profileDetail.textContent = inputDetail.value;
+  closePopup();
 }
-saveButton.addEventListener('submit', changeProfile);
-
-
+saveButton.addEventListener("submit", changeProfile);
